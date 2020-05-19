720b41305af40e6ca1fc5f5edd4eabce48f40a83
Mohand1964 <mohand.ouchia@gmail.com>
2020-05-19 10:57:39 +0200

    Ajout d'un fichier index.php au répértoire de la troisieme branche

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<script type="text/javascript" src="../commun/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="501-02-1.js"></script>
+<title>Document sans titre</title>
+</head>
+<body>
+    <button> change </button>
+    <section>
+
+    mon contenu php de la branche n°3
+    </section>
+
+</body>
+</html>
